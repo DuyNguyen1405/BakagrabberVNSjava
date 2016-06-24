900ddd7e70abe079df37137b37fcd5f77bbb1495
Duy Nguyen <duyso1000@gmail.com>
2016-06-24 19:41:16 +0700

    Imgur Api v3 test

--- src/core/ImgUpload.java
+++ src/core/ImgUpload.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -29,33 +30,42 @@ public class ImgUpload {
       for (int b; (b = in.read()) != -1; ) {
         baos.write(b);
       }
-
-      String API_URL = "http://api.imgur.com/2/upload.xml";
+      
+      //String API_URL = "http://api.imgur.com/3/upload.xml";
+      String API_URL = "http://api.imgur.com/3/image";
       String API_KEY = "4feabdcc44fea406348aedf44d1fd923";
+      String CLIENT_ID = "5bbd66e1c08cdea";
       URL apiUrl = new URL(API_URL);
-
+     
       // Encodes picture with Base64 and inserts api key
-      String data = URLEncoder.encode("image", "UTF-8") + "="
-                    + URLEncoder.encode(lib.Base64Coder.encodeLines(baos.toByteArray()), "UTF-8");
+//      String data = URLEncoder.encode("image", "UTF-8") + "="
+//              + URLEncoder.encode(imgURL, "UTF-8");
+      String data = URLEncoder.encode("image", "UTF-8") + "="  + URLEncoder.encode(lib.Base64Coder.encodeLines(baos.toByteArray()), "UTF-8");
+      //data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode(CLIENT_ID, "UTF-8");
+     // opens connection and sends data
+      HttpURLConnection  conn = (HttpURLConnection) apiUrl.openConnection();
+     conn.setDoOutput(true);
+     conn.setDoInput(true);
+     conn.setRequestMethod("POST");
+     conn.setRequestProperty("Authorization", "Client-ID " + CLIENT_ID);
+     conn.setRequestMethod("POST");
+     conn.setRequestProperty("Content-Type",
+             "applicationx-www-form-urlencoded");
 
-      data += "&" + URLEncoder.encode("key", "UTF-8") + "=" + URLEncoder.encode(API_KEY, "UTF-8");
+     conn.connect();
+     OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+     wr.write(data);
+     wr.flush();
 
-      // opens connection and sends data
-      URLConnection conn = apiUrl.openConnection();
-      conn.setDoOutput(true);
-      OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-      wr.write(data);
-      wr.flush();
-
-      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-
-      // Get direct link
-      String line;
-      line = rd.readLine();
-      line = rd.readLine();
-      directLink = StringUtils.substringBetween(line, "<original>", "</original>");
-      wr.close();
-      rd.close();
+     // Get the response
+     BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+     // Get direct link
+     String line;
+     line = rd.readLine();
+     line = rd.readLine();
+     directLink = StringUtils.substringBetween(line, "<original>", "<original>");
+     wr.close();
+     rd.close();
 
     } catch (MalformedURLException e) {
       // TODO Auto-generated catch block
